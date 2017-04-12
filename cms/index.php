f1341c28cbde9b708adec9a05745d58a3e938f9a
Niickles <vanderwaal.n@gmail.com>
2017-04-12 12:59:06 +0200

    Add files via upload
    Website toegevoegd

---
+++ cms/index.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" media="screen and (min-device-width: 1366px)" href="css/cms-styles.css">
+    </head>
+    <body>
+        <nav class="col-12">
+            <ul>
+                <li><a class="active">Dashboard</a></li>
+                <li><a>Bewerken</a></li>
+                <li><a>Content Manager</a></li>
+                <li id="uitloggen"><a>Uitloggen</a></li>
+            </ul>
+        </nav>
+        <div class="col-12 main">
+                    <div class="col-12 spacer"></div>
+                    <div class="col-2"></div>
+            <div class="col-8 dashboard">
+                <div class="col-4 block">
+                    <img src="img/edit.png" class="blockimg"><br>
+                    Pagina's bewerken
+                </div>
+                <div class="col-4 block">
+                    <img src="img/contentmanager.png" class="blockimg"><br>
+                    Bestanden
+                </div>
+                <div class="col-4 block">
+                    <img src="img/analytics.png" class="blockimg"><br>
+                    Overzicht
+                </div>
+            </div>
+                    <div class="col-2"></div>
+        </div>
+        <div class="col-12 footer">
+            <img class="footermenu settingsbtn" src="img/settings.png">
+            <img class="footermenu" src="img/help.png">
+            <img class="footermenu" src="img/webopen.png">
+        </div>
+    </body>
+</html>
